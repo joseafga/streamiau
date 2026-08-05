89ca621a27df2b67f39c63e55b3c074c4c242a4b
José Almeida <jose.afga@gmail.com>
2026-08-05 01:32:30 -0300

    Added some custom errors

--- src/streamiau/errors.cr
+++ src/streamiau/errors.cr
@@ -1,2 +1,12 @@
 class UnauthorizedError < Exception
 end
+
+error 404 do |env|
+  env.response.content_type = "text/plain"
+  "404 - O gato comeu!"
+end
+
+error 500 do |env, exception|
+  env.response.content_type = "text/plain"
+  "500 - Internal Server Error: #{exception.message}"
+end
